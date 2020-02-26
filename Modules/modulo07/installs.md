d28456fb80d661c81071fd0a94d16e682c638288
Andrelino Silva <35929943+andrelinos@users.noreply.github.com>
2020-02-26 10:54:25 -0300

    Add files via upload

---
+++ Modules/modulo07/installs.md
@@ -0,0 +1,33 @@
+# instalações
+## Dependencies
+- yarn add react-router-dom
+- yarn add react-icons
+- yarn add polished
+- yarn add global json-server
+(ver doc: https://github.com/typicode/json-server)
+- yarn add axios
+- yarn add redux
+- yarn add react-redux
+- yarn add history
+- yarn add immer
+- yarn add json-server
+- yarn add react-toastify
+- yarn add reactotron-redux-saga
+- yarn add redux-saga
+- yarn add styled-components
+
+## devDependencies
+- yarn add babel
+- yarn add babel-eslint
+- yarn add eslint
+- yarn add prettier
+
+# Executando a aplicação
+- Baixe a pasta para seu computador
+- Acesse a pastas do projeto
+- Abra o terminal execute os comandos
+- - yarn json-server server.json -p 3333
+- Agora abra outra janela do terminal (sem fechar a anterior) e execute o comando abaixo e aguarde:
+- - yarn start
+
+
---
+++ Modules/modulo07/src/pages/Cart/index.js
@@ -0,0 +1,106 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+
+import * as CartActions from '../../store/modules/cart/actions';
+
+import { Container, ProductTable, Total } from './styles';
+
+export default function Cart() {
+  /** Toda vez que for usar precisar acessar uma informação do redux,
+   * usa-se o useSelector */
+  const total = useSelector(state =>
+    formatPrice(
+      state.cart.reduce((totalSum, product) => {
+        return totalSum + product.price * product.amount;
+      }, 0)
+    )
+  );
+
+  const cart = useSelector(state =>
+    state.cart.map(product => ({
+      ...product,
+      subTotal: formatPrice(product.price * product.amount),
+    }))
+  );
+
+  /** Sempre que for uma action do redux, precisa usar dispatch */
+  const dispatch = useDispatch();
+
+  function increment(product) {
+    dispatch(CartActions.updateAmountRequest(product.id, product.amount + 1));
+  }
+
+  function decrement(product) {
+    dispatch(CartActions.updateAmountRequest(product.id, product.amount - 1));
+  }
+
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>Produto</th>
+            <th>Qtd</th>
+            <th>SubTotal</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {cart.map(product => (
+            <tr>
+              <td>
+                <img src={product.image} alt={product.title} />
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.priceFormated}</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button" onClick={() => decrement(product)}>
+                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  </button>
+                  <input type="number" readOnly value={product.amount} />
+                  <button type="button" onClick={() => increment(product)}>
+                    <MdAddCircleOutline size={20} color="#7159c1" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>{product.subTotal}</strong>
+              </td>
+              <td>
+                <button
+                  type="button"
+                  onClick={() =>
+                    dispatch(CartActions.removeFromCart(product.id))
+                  }
+                >
+                  <MdDelete size={20} color="#7159c1" />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>Total</span>
+          <strong>{total}</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
+}
